ffe656c8a75d79617b954af4ff743aaac5780f08
Kamil Nować <nowac.kamil@gmail.com>
2025-07-12 11:07:01 +0200

    [chore] Add a bit of documentation for showRegionMapProvider

--- packages/feature_show_map/lib/presentation/di/shared_providers.dart
+++ packages/feature_show_map/lib/presentation/di/shared_providers.dart
@@ -7,10 +7,15 @@ import 'package:shared/interfaces/feature_show_map/widgets/show_map.dart';
 
 part 'shared_providers.g.dart';
 
+/// Provider to show a region map.
 @riverpod
 ShowMap showRegionMap(Ref ref) =>
     () => const RegionMap();
 
+/// Provider override for [showRegionMap]. It works as dependency injection in
+/// riverpod. All overrides should be added to ProviderScope in main.dart.
+/// The origin is [getRegionMap] the dummy provider in
+/// [package:shared/feature_show_map/widgets/show_map.dart]
 final showRegionMapProviderOverride = ProviderOverride(
   origin: getRegionMapProvider,
   override: showRegionMapProvider,
